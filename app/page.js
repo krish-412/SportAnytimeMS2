330d0db5c5155d129b9806a4faf3a1444381f9fc
krish-412 <vksg2023@gmail.com>
2026-06-29 01:43:39 +0800

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>SportAnytime MVP</h1>
+      <p>Frontend is successfully deployed and running!</p>
+    </main>
+  );
+}
